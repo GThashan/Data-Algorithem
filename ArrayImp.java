766c1f444aacee959cdb793086a04b04c9af592b
GThashan <124758477+tharindususl@users.noreply.github.com>
2024-08-12 14:34:29 +0530

    Array

---
+++ ArrayImp.java
@@ -0,0 +1,13 @@
+public class ArrayImp {
+
+    public static void main(String[] args) {
+        int [] arr = {10,20,30,40,50,60};
+        
+        // System.out.println(arr[2]);
+        // System.out.println(arr[1]+arr[2]);
+
+        for(int i=0; i<arr.length ; i++){
+            System.out.println(arr[i]);
+        }
+    }
+}

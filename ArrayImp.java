18ccfc17ce265476f1f377586c40f099ccdb9f2b
GThashan <124758477+tharindususl@users.noreply.github.com>
2024-08-12 14:49:33 +0530

    get input

--- ArrayImp.java
+++ ArrayImp.java
@@ -1,13 +1,24 @@
+import java.util.Scanner;
+
 public class ArrayImp {
 
     public static void main(String[] args) {
-        int [] arr = {10,20,30,40,50,60};
-        
-        // System.out.println(arr[2]);
-        // System.out.println(arr[1]+arr[2]);
+        int [] arr = new int[5];
+        int sum = 5;
 
-        for(int i=0; i<arr.length ; i++){
+        Scanner userI = new Scanner(System.in);
+
+        for(int j=0; j<sum;j++){
+           arr[j]= userI.nextInt();
+           
+        }
+
+     
+        
+        for(int i=0; i<sum ; i++){
             System.out.println(arr[i]);
         }
+
+        
     }
 }
